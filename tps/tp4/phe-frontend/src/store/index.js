7c1377694faf4e5b909e5c355e3e87396753cb66
Corentin Latappy <corentin.latappy@promyze.com>
2023-10-06 08:04:20 +0200

    ✨ Ajout Cours 4 & TP4

---
+++ tps/tp4/phe-frontend/src/store/index.js
@@ -0,0 +1,17 @@
+import { createStore } from 'vuex';
+
+export const store = new createStore({
+    state: {
+        loggedin: true,
+        username: '',
+    },
+    mutations: {
+        // TODO
+    },
+    actions: {
+        // TODO
+    },
+    getters: {
+        // TODO
+    },
+});
